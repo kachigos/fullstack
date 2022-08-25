706e74940c0834961af0653f54f1393840dd8bf7
kachigos <kachigos@C02WF085HV2T.grubhub.local>
2022-08-25 13:58:03 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=int)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS")
 
 
 
