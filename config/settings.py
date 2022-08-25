1f8dd2802e46ae3af5e657f946a8ce3ef8d91000
kachigos <kachigos@C02WF085HV2T.grubhub.local>
2022-08-25 13:55:04 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-# from decouple import config
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c4j0k&_v1d@y8%ncj#0r2cav+lc#rjz8)i1#4t4nw^gz1cy0t'
+
+# SECRET_KEY = 'django-insecure-(c4j0k&_v1d@y8%ncj#0r2cav+lc#rjz8)i1#4t4nw^gz1cy0t'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG',cast=int)
 
-ALLOWED_HOSTS = ['127.0.0.1,localhost ']
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
 
